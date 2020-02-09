701e1bcef681e89a40f7a530dcac41715f2de8ed
azxj <1158257471@qq.com>
2020-02-09 23:49:40 +0800

    build: add jest
    - Add jest to provide testing framework
    - Add jest.config.js to configure jest
    - Enable jest environment for eslint to avoid "no-undef" error

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+};
