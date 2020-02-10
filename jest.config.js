911277f8f78710862ef238c20a7ce1c1e58bbca7
azxj <1158257471@qq.com>
2020-02-10 16:23:04 +0800

    build: collect coverage information when executing the test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
