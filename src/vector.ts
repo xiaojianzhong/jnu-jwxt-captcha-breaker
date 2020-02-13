3fcb66fa8bf5822e1c4c586b2c37d1850ae9f337
azxj <1158257471@qq.com>
2020-02-13 17:17:23 +0800

    feat(vector): add vector type definitions

---
+++ src/vector.ts
@@ -0,0 +1,6 @@
+export type Vector1D = number[];
+export type Vector2D = number[][];
+export type Vector3D = number[][][];
+export type Vector4D = number[][][][];
+export type Vector5D = number[][][][][];
+export type Vector6D = number[][][][][][];
