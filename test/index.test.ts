da660d29f8e577208f12315f7f54df6ea64ef8bf
azxj <1158257471@qq.com>
2020-02-14 23:41:58 +0800

    refactor(index): remove `() => undefined`
    Use `.mockImplementation()` to replace `.mockImplementation(() => undefined)`.

--- test/index.test.ts
+++ test/index.test.ts
@@ -27,10 +27,10 @@ describe('index', () => {
       let modelSave: jest.SpyInstance;
 
       beforeEach(() => {
-        modelLoad = jest.spyOn(breaker['model'], 'load');
-        dataLoad = jest.spyOn(breaker['data'], 'load');
-        modelTrain = jest.spyOn(breaker['model'], 'train');
-        modelSave = jest.spyOn(breaker['model'], 'save');
+        modelLoad = jest.spyOn(breaker['model'], 'load').mockImplementation();
+        dataLoad = jest.spyOn(breaker['data'], 'load').mockImplementation();
+        modelTrain = jest.spyOn(breaker['model'], 'train').mockImplementation();
+        modelSave = jest.spyOn(breaker['model'], 'save').mockImplementation();
       });
 
       afterEach(() => {
@@ -41,51 +41,26 @@ describe('index', () => {
       });
 
       it('should load the model from the file system', async () => {
-        modelLoad.mockImplementation(async () => undefined);
-        dataLoad.mockImplementation(async () => undefined);
-        modelTrain.mockImplementation(async () => undefined);
-        modelSave.mockImplementation(async () => undefined);
-
         await breaker.init({ loadModel: true });
 
         expect(modelLoad).toBeCalled();
       });
       it('should train the model', async () => {
-        modelLoad.mockImplementation(() => undefined);
-        dataLoad.mockImplementation(() => undefined);
-        modelTrain.mockImplementation(() => undefined);
-        modelSave.mockImplementation(() => undefined);
-
         await breaker.init({ trainModel: true });
 
         expect(modelTrain).toBeCalled();
       });
       it('should save the model', async () => {
-        modelLoad.mockImplementation(() => undefined);
-        dataLoad.mockImplementation(() => undefined);
-        modelTrain.mockImplementation(() => undefined);
-        modelSave.mockImplementation(() => undefined);
-
         await breaker.init({ saveModel: true });
 
         expect(modelSave).toBeCalled();
       });
       it('should load the model from the specific directory', async () => {
-        modelLoad.mockImplementation(() => undefined);
-        dataLoad.mockImplementation(() => undefined);
-        modelTrain.mockImplementation(() => undefined);
-        modelSave.mockImplementation(() => undefined);
-
         await breaker.init({ loadModel: true, modelDir: '/path/to/the/model' });
 
         expect(modelLoad).toBeCalledWith('/path/to/the/model');
       });
       it('should save the model to the specific directory', async () => {
-        modelLoad.mockImplementation(() => undefined);
-        dataLoad.mockImplementation(() => undefined);
-        modelTrain.mockImplementation(() => undefined);
-        modelSave.mockImplementation(() => undefined);
-
         await breaker.init({ saveModel: true, modelDir: '/path/to/the/model' });
 
         expect(modelSave).toBeCalledWith('/path/to/the/model');
